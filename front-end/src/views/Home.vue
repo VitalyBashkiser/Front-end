8fcce09c8756584a390b7cf4b8278d1f05cb743a
Vitalii Bashkiser <vitalikbashkiser@gmail.com>
2023-09-23 04:01:40 +0300

    Add .env & .env.sample, add Home.vue, update README.md,

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <header>
+      <h1>WELCOME</h1>
+    </header>
+    <main>
+      <article>
+        <p>Let's get started</p>
+      </article>
+    </main>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
