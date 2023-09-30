e5dc430c52d391e8456c5f802e4d3a9e393bae29
Vitalii Bashkiser <vitalikbashkiser@gmail.com>
2023-09-30 21:23:53 +0300

    "Implemented user registration using Auth0, integrated token storage in Vuex and localStorage."

--- front-end/src/components/UserRegistration.vue
+++ front-end/src/components/UserRegistration.vue
@@ -1,12 +1,46 @@
 <template>
   <div>
-    <!-- Contents of the UserRegistration template -->
+    <input v-model="email" @blur="validateEmail" placeholder="Email" />
+    <span v-if="emailError">{{ emailError }}</span>
+
+    <input v-model="password" placeholder="Password" type="password" />
+    <input v-model="confirmPassword" @blur="validatePassword" placeholder="Confirm Password" type="password" />
+    <span v-if="passwordError">{{ passwordError }}</span>
+
+    <button @click="register">Register</button>
   </div>
 </template>
 
 <script>
 export default {
-  // UserRegistration component logic
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      emailError: '',
+      passwordError: '',
+    };
+  },
+  methods: {
+    validateEmail() {
+      if (!this.email.includes('@')) {
+        this.emailError = 'Invalid email address';
+      } else {
+        this.emailError = '';
+      }
+    },
+    validatePassword() {
+      if (this.password !== this.confirmPassword) {
+        this.passwordError = 'Passwords do not match';
+      } else {
+        this.passwordError = '';
+      }
+    },
+    register() {
+      // Realization of registration
+    },
+  },
 };
 </script>
 
--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -3,19 +3,23 @@ import { createStore } from 'vuex';
 export default createStore({
   state: {
     testString: 'Hello, Vuex!',
+    user: null, // Add a property to store user data
   },
   getters: {
     getTestString: state => state.testString,
+    getUser: state => state.user, // Getter for user data retrieval
   },
   actions: {
-  updateTestString: ({ commit }, newString) => {
-    commit('setTestString', newString);
+    updateTestString: ({ commit }, newString) => {
+      commit('setTestString', newString);
     },
   },
   mutations: {
     setTestString: (state, newString) => {
       state.testString = newString;
     },
+    setUser: (state, user) => {
+      state.user = user;
+    },
   },
 });
-
